d55895ec64b4f1d21a979919c04f2d73c6fe82d8
zqyadam <zqyadam@qq.com>
2016-12-11 16:36:36 +0800

    update HoleAxisLength
    更新孔、轴及长度页面

---
+++ data/HoleAxisLengthData.js
@@ -0,0 +1,491 @@
+var HoleAxisLengthData = [{
+	"dimension": [0.1,0.3],
+	"condition":["=","="],
+	"D":{
+		"I":{
+			"upper":0.05,
+			"lower":0
+		},
+		"II":{
+			"upper":0.06,
+			"lower":0
+		}
+	},
+	"d":{
+		"I":{
+			"upper":0,
+			"lower":-0.05
+		},
+		"II":{
+			"upper":0,
+			"lower":-0.06
+		}
+	},
+	"L":0.05
+},{
+	"dimension": [0.3,0.6],
+	"condition":[">","="],
+	"D":{
+		"I":{
+			"upper":0.06,
+			"lower":0
+		},
+		"II":{
+			"upper":0.08,
+			"lower":0
+		}
+	},
+	"d":{
+		"I":{
+			"upper":0,
+			"lower":-0.06
+		},
+		"II":{
+			"upper":0,
+			"lower":-0.08
+		}
+	},
+	"L":0.1
+},{
+	"dimension": [0.6,1],
+	"condition":[">","<"],
+	"D":{
+		"I":{
+			"upper":0.07,
+			"lower":0
+		},
+		"II":{
+			"upper":0.11,
+			"lower":0
+		}
+	},
+	"d":{
+		"I":{
+			"upper":0,
+			"lower":-0.07
+		},
+		"II":{
+			"upper":0,
+			"lower":-0.11
+		}
+	},
+	"L":0.1
+},{
+	"dimension": [1,3],
+	"condition":["=","="],
+	"D":{
+		"I":{
+			"upper":0.1,
+			"lower":0
+		},
+		"II":{
+			"upper":0.14,
+			"lower":0
+		}
+	},
+	"d":{
+		"I":{
+			"upper":0,
+			"lower":-0.1
+		},
+		"II":{
+			"upper":0,
+			"lower":-0.14
+		}
+	},
+	"L":0.12
+},{
+	"dimension": [3,6],
+	"condition":[">","="],
+	"D":{
+		"I":{
+			"upper":0.12,
+			"lower":0
+		},
+		"II":{
+			"upper":0.18,
+			"lower":0
+		}
+	},
+	"d":{
+		"I":{
+			"upper":0,
+			"lower":-0.12
+		},
+		"II":{
+			"upper":0,
+			"lower":-0.18
+		}
+	},
+	"L":0.15
+},{
+	"dimension": [6,10],
+	"condition":[">","="],
+	"D":{
+		"I":{
+			"upper":0.15,
+			"lower":0
+		},
+		"II":{
+			"upper":0.22,
+			"lower":0
+		}
+	},
+	"d":{
+		"I":{
+			"upper":0,
+			"lower":-0.15
+		},
+		"II":{
+			"upper":0,
+			"lower":-0.22
+		}
+	},
+	"L":0.18
+},{
+	"dimension": [10,18],
+	"condition":[">","="],
+	"D":{
+		"I":{
+			"upper":0.18,
+			"lower":0
+		},
+		"II":{
+			"upper":0.27,
+			"lower":0
+		}
+	},
+	"d":{
+		"I":{
+			"upper":0,
+			"lower":-0.18
+		},
+		"II":{
+			"upper":0,
+			"lower":-0.27
+		}
+	},
+	"L":0.21
+},{
+	"dimension": [18,30],
+	"condition":[">","="],
+	"D":{
+		"I":{
+			"upper":0.21,
+			"lower":0
+		},
+		"II":{
+			"upper":0.33,
+			"lower":0
+		}
+	},
+	"d":{
+		"I":{
+			"upper":0,
+			"lower":-0.21
+		},
+		"II":{
+			"upper":0,
+			"lower":-0.33
+		}
+	},
+	"L":0.26
+},{
+	"dimension": [30,50],
+	"condition":[">","="],
+	"D":{
+		"I":{
+			"upper":0.25,
+			"lower":0
+		},
+		"II":{
+			"upper":0.39,
+			"lower":0
+		}
+	},
+	"d":{
+		"I":{
+			"upper":0,
+			"lower":-0.25
+		},
+		"II":{
+			"upper":0,
+			"lower":-0.39
+		}
+	},
+	"L":0.31
+},{
+	"dimension": [50,80],
+	"condition":[">","="],
+	"D":{
+		"I":{
+			"upper":0.3,
+			"lower":0
+		},
+		"II":{
+			"upper":0.46,
+			"lower":0
+		}
+	},
+	"d":{
+		"I":{
+			"upper":0,
+			"lower":-0.3
+		},
+		"II":{
+			"upper":0,
+			"lower":-0.46
+		}
+	},
+	"L":0.37
+},{
+	"dimension": [80,120],
+	"condition":[">","="],
+	"D":{
+		"I":{
+			"upper":0.35,
+			"lower":0
+		},
+		"II":{
+			"upper":0.54,
+			"lower":0
+		}
+	},
+	"d":{
+		"I":{
+			"upper":0,
+			"lower":-0.35
+		},
+		"II":{
+			"upper":0,
+			"lower":-0.54
+		}
+	},
+	"L":0.43
+},{
+	"dimension": [120,180],
+	"condition":[">","="],
+	"D":{
+		"I":{
+			"upper":0.4,
+			"lower":0
+		},
+		"II":{
+			"upper":0.63,
+			"lower":0
+		}
+	},
+	"d":{
+		"I":{
+			"upper":0,
+			"lower":-0.4
+		},
+		"II":{
+			"upper":0,
+			"lower":-0.63
+		}
+	},
+	"L":0.5
+},{
+	"dimension": [180,250],
+	"condition":[">","="],
+	"D":{
+		"I":{
+			"upper":0.46,
+			"lower":0
+		},
+		"II":{
+			"upper":0.72,
+			"lower":0
+		}
+	},
+	"d":{
+		"I":{
+			"upper":0,
+			"lower":-0.46
+		},
+		"II":{
+			"upper":0,
+			"lower":-0.72
+		}
+	},
+	"L":0.57
+},{
+	"dimension": [250,315],
+	"condition":[">","="],
+	"D":{
+		"I":{
+			"upper":0.52,
+			"lower":0
+		},
+		"II":{
+			"upper":0.81,
+			"lower":0
+		}
+	},
+	"d":{
+		"I":{
+			"upper":0,
+			"lower":-0.52
+		},
+		"II":{
+			"upper":0,
+			"lower":-0.81
+		}
+	},
+	"L":0.65
+},{
+	"dimension": [315,400],
+	"condition":[">","="],
+	"D":{
+		"I":{
+			"upper":0.57,
+			"lower":0
+		},
+		"II":{
+			"upper":0.89,
+			"lower":0
+		}
+	},
+	"d":{
+		"I":{
+			"upper":0,
+			"lower":-0.57
+		},
+		"II":{
+			"upper":0,
+			"lower":-0.89
+		}
+	},
+	"L":0.7
+},{
+	"dimension": [400,500],
+	"condition":[">","="],
+	"D":{
+		"I":{
+			"upper":0.63,
+			"lower":0
+		},
+		"II":{
+			"upper":0.97,
+			"lower":0
+		}
+	},
+	"d":{
+		"I":{
+			"upper":0,
+			"lower":-0.63
+		},
+		"II":{
+			"upper":0,
+			"lower":-0.97
+		}
+	},
+	"L":0.77
+},{
+	"dimension": [500,630],
+	"condition":[">","="],
+	"D":{
+		"I":{
+			"upper":0.7,
+			"lower":0
+		},
+		"II":{
+			"upper":1.1,
+			"lower":0
+		}
+	},
+	"d":{
+		"I":{
+			"upper":0,
+			"lower":-0.7
+		},
+		"II":{
+			"upper":0,
+			"lower":-1.1
+		}
+	},
+	"L":0.87
+},{
+	"dimension": [630,800],
+	"condition":[">","="],
+	"D":{
+		"I":{
+			"upper":0.8,
+			"lower":0
+		},
+		"II":{
+			"upper":1.25,
+			"lower":0
+		}
+	},
+	"d":{
+		"I":{
+			"upper":0,
+			"lower":-0.8
+		},
+		"II":{
+			"upper":0,
+			"lower":-1.25
+		}
+	},
+	"L":1
+}]
+
+
+
+
+
+
+
+(function (window) {
+	// 定义获取行索引
+	var basicSize;
+	
+	function HoleAxisLength(bs){
+		this.basicSize = bs;
+		
+	}
+	
+	function getIndex(){
+		for (let i = 0 ; i< HoleAxisLengthData.length; i++) {
+			var temp = HoleAxisLengthData[i];
+			// min size
+			
+			
+			if (basicSize < temp['dimension'][1]) {
+				
+			}
+			switch (temp['condition']){
+				case value:
+					break;
+				default:
+					break;
+			}
+			var state = this.basicSize + temp['condition'] + temp[] 
+			if (eval( >temp['dimension']) {
+				
+			}
+		}
+	}
+	
+	function getLevelIHole () {
+		
+	}
+	
+	function getLevelIAxis () {
+		
+	}
+	
+	function getLevelIIHole () {
+		
+	}
+	
+	function getLevelIIAxis () {
+		
+	}
+	
+	function getLength () {
+		
+	}
+}(window))
